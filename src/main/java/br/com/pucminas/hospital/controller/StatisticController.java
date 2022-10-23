d65600ddc6f7439f905905f47ce8d26cb4823bd4
Guilherme Santos <guilherme.rfs00@gmail.com>
2022-10-23 18:53:53 -0300

    Ajustes TODO

--- src/main/java/br/com/pucminas/hospital/controller/StatisticController.java
+++ src/main/java/br/com/pucminas/hospital/controller/StatisticController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/statistic")
-public class statisticController {
+public class StatisticController {
 
     @Autowired
     StatisticService service;
